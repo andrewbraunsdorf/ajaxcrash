92fef3f4b927aff2bd31df68813c4d80e0d42ec4
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-07 02:01:11 +0000

    added Post form and AJAX Post Form

--- process.php
+++ process.php
@@ -2,7 +2,12 @@
 
 echo 'processing...';
 
+//Check for POST variable
+if(isset($_POST['name'])){
+	echo "POST: Your name is ". $_POST['name'];
+}
+
 //Check for GET variable
 if(isset($_GET['name'])){
-	echo "GET: Your name is ", $_GET['name'];
+	echo "GET: Your name is ". $_GET['name'];
 }
